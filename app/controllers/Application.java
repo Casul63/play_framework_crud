af6f0190ae4ddd966dc6189a4a43e352bfbb82bf
Casul63 <ikhsanhafidz09@gmail.com>
2023-11-12 12:58:37 +0700

    Menyelesaikan Semua Fitur

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -19,10 +19,44 @@ public class Application extends Controller {
         render(daftar);
     }
 
+    public static void create_pemilik(){
+        render();
+    }
+
     public static void store_menu(Makanan makanan){
-        makanan = makanan.merge();
-        makanan.save();
+        makanan.merge();
         index();
     }
 
+    public static void store_pemilik(PemilikMakanan pemilik){
+        pemilik.save();
+        pemilik_makanan();
+    }
+    public static void pemilik_makanan(){
+        List daftar = PemilikMakanan.findAll();
+        render(daftar);
+    }
+
+    public static void delete_menu(Long id){
+        Makanan makanan = Makanan.find("id=?1", id).first();
+        makanan.delete();
+        index();
+    }
+
+    public static void delete_pemilik(Long id){
+        PemilikMakanan pemilik_lama = PemilikMakanan.find("id=?1", id).first();
+        pemilik_lama.delete();
+        pemilik_makanan();
+    }
+
+    public static void edit_menu(Long id){
+        Makanan makanan_lama = Makanan.find("id=?1", id).first();
+        List daftar = PemilikMakanan.findAll();
+        render(makanan_lama, daftar);
+    }
+    
+    public static void edit_pemilik(Long id){
+        PemilikMakanan pemilik_lama = PemilikMakanan.find("id=?1", id).first();
+        render(pemilik_lama);
+    }
 }
